4f0165862be4b36c8cc497e99110228a63bf8848
Nicolas Blanco <nicolasblancodev@gmail.com>
2024-10-11 18:15:45 -0300

    agregamos variables de entorno

--- practica.js
+++ practica.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 //Le damos instrucciones a nuestra computadora/servidor para que quede escuchando en determinado puerto.
 //Modulo Http, te permite gesionar todas las interacciones de red a traves de internet con una PC.
 //Lo que hace es traer codigo de una libreria,ahi podemos usar un metodo para darle instrucciones a la pc.
@@ -11,5 +13,9 @@ function requestController(){
 //  Configurar nuestro servidor.Acá le decimos que va a pasar cada vez que se reciba una request.
 const server = http.createServer(requestController); //Creamos un servidor para controlar las peticiones,Cada vez que llega una request a nuestro servidor ejecuta esa funcion que pasamos como parametro en createServer,controla las peticiones que llegana nuestro servidor y controlarla a gusto,como querramos,cada vez que llega una request se ejecuta la funcion como parametro y su lógica.
 
+const PORT = process.env.PORT
+
 //Ponemos a escuchar a nuestro servidor en un determinado puerto.
-server.listen(4000);
+server.listen(PORT, function() {
+    console.log("Aplicacion corriendo en puerto: " + PORT)
+});
